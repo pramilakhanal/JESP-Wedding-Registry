f0c62c24df021f7d5999519992d16adfe209429d
ericgoldstein <bergerd306@aol.com>
2018-03-07 20:52:52 -0700

    ready to deploy

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
-  password: "sam067581968",
+  password: "bergerd306",
   database: "wedding_db"
 });
 
