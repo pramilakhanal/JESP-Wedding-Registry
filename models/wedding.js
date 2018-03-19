446b51336648c6e816d52c59e561ce2db93292f3
Jeffrey Phelps <phelpsjeffrey@yahoo.com>
2018-03-18 19:48:01 -0600

    Beautifying Code

--- models/wedding.js
+++ models/wedding.js
@@ -1,28 +1,28 @@
+// Eric Goldstein, Pramila Khanal, Sam Bouzroud, Jeffrey Phelps - 
+//  DU Web Dev Bootcamp 2017/2018 - Mod-17 Homework - Project 2
 
 var orm = require("../config/orm.js");
 
- var weddingRegistry = {
-  all: function(callback) {
-    orm.selectAll("wedding", function(res) {
+var weddingRegistry = {
+  all: function (callback) {
+    orm.selectAll("wedding", function (res) {
       callback(res);
     });
   },
 
-  create: function(req, callback)
-  {
-  	orm.insertOne("wedding", req, function(res) {
-  		callback(res);
-  	});
+  create: function (req, callback) {
+    orm.insertOne("wedding", req, function (res) {
+      callback(res);
+    });
   },
 
-  update: function(objColVals, condition, callback) {
-    orm.updateOne("wedding", objColVals, condition, function(res) {
+  update: function (objColVals, condition, callback) {
+    orm.updateOne("wedding", objColVals, condition, function (res) {
       callback(res);
     });
   }
-  
+
 };
 
 // Export the database functions for the controller (catsController.js).
 module.exports = weddingRegistry;
-
--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,14 +1,11 @@
-(function($){
-  $(function(){
+// Eric Goldstein, Pramila Khanal, Sam Bouzroud, Jeffrey Phelps - 
+//  DU Web Dev Bootcamp 2017/2018 - Mod-17 Homework - Project 2
+
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-
-
-
-
-
