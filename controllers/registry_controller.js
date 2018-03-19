d4b96fd0f657f0cdab69defc817aa2560f21c443
Jeffrey Phelps <phelpsjeffrey@yahoo.com>
2018-03-18 19:46:10 -0600

    Add files via upload
    Beautifying Code

--- controllers/registry_controller.js
+++ controllers/registry_controller.js
@@ -1,11 +1,14 @@
+// Eric Goldstein, Pramila Khanal, Sam Bouzroud, Jeffrey Phelps - 
+//  DU Web Dev Bootcamp 2017/2018 - Mod-17 Homework - Project 2
+
 var express = require('express');
 var wedding = require('../models/wedding.js');
 var router = express.Router();
 var path = require('path');
 // Create all our routes
 
-router.get("/", function(req, res) {
-	wedding.all(function(data) {
+router.get("/", function (req, res) {
+	wedding.all(function (data) {
 		var weddingInfo = {
 			wedding: data
 		};
@@ -16,16 +19,16 @@ router.get("/", function(req, res) {
 	});
 });
 
-router.get("/signup", function(req, res) {
+router.get("/signup", function (req, res) {
 	res.sendFile(path.join(__dirname, '../public/assets/js/Signup.html'));
 });
 
- router.post("/signup", function(req, res) {
- 	res.render("index");
- });
+router.post("/signup", function (req, res) {
+	res.render("index");
+});
 
-router.get("/authenticated", function(req, res) {
-	wedding.all(function(data) {
+router.get("/authenticated", function (req, res) {
+	wedding.all(function (data) {
 		var weddingInfo = {
 			wedding: data
 		};
@@ -39,8 +42,8 @@ router.get("/authenticated", function(req, res) {
 
 
 
-router.get("/guestlist", function(req, res) {
-	wedding.all(function(data) {
+router.get("/guestlist", function (req, res) {
+	wedding.all(function (data) {
 		var weddingInfo = {
 			wedding: data
 		};
@@ -53,78 +56,92 @@ router.get("/guestlist", function(req, res) {
 
 
 
-router.put("/:id/:update", function(req, res) {
-		var item = {id: req.params.id, condition: req.params.update};
-		//console.log("condition", condition);
+router.put("/:id/:update", function (req, res) {
+	var item = {
+		id: req.params.id,
+		condition: req.params.update
+	};
+	//console.log("condition", condition);
 
-		if (item.condition === "0") {
-			wedding.update({reserved: true}, {id: item.id}, function() {
+	if (item.condition === "0") {
+		wedding.update({
+			reserved: true
+		}, {
+			id: item.id
+		}, function () {
 
-			});
-		}
-		else {
-			wedding.update({reserved: false}, item.id, function() {
-			});
-		}
-		wedding.all(function(data) {
+		});
+	} else {
+		wedding.update({
+			reserved: false
+		}, item.id, function () {});
+	}
+	wedding.all(function (data) {
+		var weddingInfo = {
+			wedding: data
+		};
+
+		// console.log(weddingInfo);
+
+		res.json(data);
+	});
+
+});
+
+
+
+
+router.put("/guestlist/:id/:update", function (req, res) {
+	var item = {
+		id: req.params.id,
+		condition: req.params.update
+	};
+	// 		//console.log("condition", condition);
+
+	if (item.condition === "0") {
+		wedding.update({
+			reserved: true
+		}, {
+			id: item.id
+		}, function () {
+
+		});
+	} else {
+		wedding.update({
+			reserved: false
+		}, item.id, function () {});
+	}
+	wedding.all(function (data) {
+
+		var weddingInfo = {
+			wedding: data
+		};
+
+		// 			// console.log(weddingInfo);
+
+		res.json(data);
+	});
+
+});
+
+router.post("/", function (req, res) {
+
+	console.log(req.body);
+
+	wedding.create(req.body, function () {
+		wedding.all(function (data) {
 			var weddingInfo = {
 				wedding: data
 			};
 
-			// console.log(weddingInfo);
-
 			res.json(data);
 		});
-
 	});
+});
 
 
 
 
- router.put("/guestlist/:id/:update", function(req, res) {
- 		var item = {id: req.params.id, condition: req.params.update};
-// 		//console.log("condition", condition);
 
- 		if (item.condition === "0") {
-			wedding.update({reserved: true}, {id: item.id}, function() {
-
-			});
-		}
-		else {
-			wedding.update({reserved: false}, item.id, function() {
-			});
-		}
-		 		wedding.all(function(data) {
-
-		 		var weddingInfo = {
-				wedding: data
- 			};
-
-// 			// console.log(weddingInfo);
-
- 			res.json(data);
-	});
-
- });
-
-	router.post("/", function(req, res) {
-
-		console.log(req.body);
-
-		wedding.create(req.body, function(){
-			wedding.all(function(data) {
-			var weddingInfo = {
-				wedding: data
-			};
-
-			res.json(data);
-			});
-		});
-	});
-	
-
-
-	
-
-	// Export routes for server.js to use.
-	module.exports = router;
+// Export routes for server.js to use.
+module.exports = router;
